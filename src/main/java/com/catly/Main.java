d68a6185ced1f5056e446cf83ee65cb167570a23
yelinkim <dpfls66@digitalzone.co.kr>
2023-05-31 13:24:31 +0900

    첫커밋

---
+++ src/main/java/com/catly/Main.java
@@ -0,0 +1,7 @@
+package com.catly;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/catly/springdev/SpringDevApplication.java
@@ -0,0 +1,12 @@
+package com.catly.springdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringDevApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDevApplication.class, args);
+    }
+}
